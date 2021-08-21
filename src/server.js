6e0e9f8cb7a1135e7553b0797bf9dc23b65634df
realies <5107843+realies@users.noreply.github.com>
2021-08-21 17:28:21 +0300

    add arguments check, usage message, optional repository folder

--- src/server.js
+++ src/server.js
@@ -4,10 +4,16 @@ import getUserLikes from './services/getUserLikes';
 import getMissingMusic from './services/getMissingMusic';
 
 (async () => {
-  const username = process.argv.slice(',').pop();
+  const args = process.argv.slice(',');
+  if (args.length !== 3 && args.length !== 4) {
+    logger.error(`Usage: yarn start username [folder]`);
+    process.exit(1);
+  }
+  const username = args[2];
+  const folder = args[3];
   const client = await getClient(username);
   logger.info(`Getting latest likes for ${username}`);
   const userLikes = await getUserLikes(client, 0, 50);
-  const newMusic = await getMissingMusic(userLikes);
+  const newMusic = await getMissingMusic(userLikes, folder);
   newMusic.map(item => logger.info(`Added ${item.track}`));
 })();
